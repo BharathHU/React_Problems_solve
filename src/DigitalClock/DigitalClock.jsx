574ad63312d70bb62d1ca5082cb80d0ee358371f
BharathHU <bharathhubharath@gmail.com>
2026-07-16 12:50:28 +0530

    updated

--- src/DigitalClock/DigitalClock.jsx
+++ src/DigitalClock/DigitalClock.jsx
@@ -1,40 +1,63 @@
 import React,{useState,useEffect} from 'react'
 
 function DigitalClock() {
-    const[time,setTime]=useState(new Date());
-   useEffect(() => {
-    // Set up the 1-second interval
-    const timerId = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
+  const[data,setData]=useState([])
+  const[error,setError]=useState('')
 
-    // Clean up interval on component unmount
-    return () => clearInterval(timerId);
-  }, []);
+  useEffect(()=>{
+    const ujwal=async()=>{
+      try{
+      const fetchData=await fetch('https://dummyjson.com/products')
+      const respone=await fetchData.json()
+      setData(respone.products)
+    }
+    catch(error){
+      console.log("Error Fetching Data",error)
+      setError('Error Fetching Data')
+
+    }
+  }
+  ujwal()
+  },[])
   return (
-    <div style={styles.container}>
-      <h2>Real-Time Clock</h2>
-      <div style={styles.clock}>
-        {time.toLocaleTimeString()}
-      </div>
+    <div
+  style={{
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    gap: "20px",
+    backgroundImage:
+      'url(https://img.freepik.com/premium-photo/ultra-realistic-orange-background-4k-hd-photo-product_1193781-21526.jpg)',
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    minHeight: "100vh",
+    padding: "20px",
+    backgroundAttachment:'fixed'
+  }}
+>{
+        data.map((value,id)=>(
+          <div key={value.id} style={{border:'1px solid black',height:'350px',width:'300px',borderRadius:'10px', boxShadow:'0px 0px 10px green',backgroundColor:'whitesmoke'}}>
+             <img
+            src={value.thumbnail}
+            alt={value.title}
+            style={{
+              width: "90px",
+              height: "150px",
+              objectFit: "cover",
+            }}
+          />
+            <p>Name:{value.title} <br />
+            Price:₹ {value.price*90} <br />
+            Rating:{value.rating} <br />
+          {value.description.substring(0,40)}</p>
+          </div>
+         
+          
+        ))
+      }
     </div>
   )
 }
 
-const styles = {
-  container: {
-    padding: '20px',
-    border: '1px solid #ccc',
-    borderRadius: '50%',
-    margin: '10px 0',
-    textAlign: 'center',
-    backgroundColor: 'green',
-  },
-  clock: {
-    fontSize: '2rem',
-    fontWeight: 'bold',
-    fontFamily: 'monospace',
-    color: '#333',
-  }
-};
 export default DigitalClock
